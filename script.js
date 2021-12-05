b6d812bfa69091321c1810c9c0f7659af37b932f
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-12-06 04:03:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.getElementById("passfield").addEventListener("click",function(){
   var x= document.getElementById("pass")
+    if(x.type=="password"){
+    x.type="text";
+  }else{
+    x.type="password";
+  }
+});
   
   
   
